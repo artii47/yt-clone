48c6acd1cb788962a95397c9aed9afbaf277c921
artas <artasss47@gmail.com>
2020-04-05 20:25:03 +0200

    deleted unneeded files

--- src/components/header/header-right-items/header-right-items.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import * as Styled from "./header-right-items.styles";
-
-const HeaderRightItems = () => {
-  return <Styled.HeaderRightItems></Styled.HeaderRightItems>;
-};
-
-export default HeaderRightItems;
